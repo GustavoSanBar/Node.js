4345843d9ec9f31f70531d31f5bd06ff8e16278f
GustavoSanBar <99770511+GustavoSanBar@users.noreply.github.com>
2023-03-14 19:18:19 -0300

    Modulos

---
+++ 2-Modulos/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./soma")
+var SubFunc = require("./sub")
+var MultFunc = require("./mult")
+var DiviFunc = require("./divi")
+
+console.log(SomaFunc(10,10))
+console.log(SubFunc(100,50))
+console.log(MultFunc(100,10))
+console.log(DiviFunc(2,2))
