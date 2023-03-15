d95cee092de61c228ae28531a0aeefe7b545abb7
GustavoSanBar <99770511+GustavoSanBar@users.noreply.github.com>
2023-03-15 19:10:31 -0300

    install nodemon

---
+++ 4-Rotas-Dinamicas/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get("/", function(req, res){
+    res.send("Seja bem vindo ao app!")
+})
+app.get('/ola/:nome/:cargo',function(req, res){
+    res.send("<h1>Ola " +req.params.nome + "</h1>"+"<h2> Voce quer ser um  "+ req.params.cargo + "</h2>" )
+})
+app.listen(6969, function(){
+    console.log("Servidor rodando na url http://localhost:6969")
+})
